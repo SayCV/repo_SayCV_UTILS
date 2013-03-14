161f991964eb1d105c1255deba542ab8988c1d61
SayCV <sssxyc@163.com>
2013-03-14 23:22:37 +0800

    update: build env .

--- MXE_Build_OpenVPN.bat
+++ MXE_Build_OpenVPN.bat
@@ -24,39 +24,42 @@ set /a EOF_ENV_FLAG=0
 set INSTALL_ENV_DIR_MINGW=D:\MinGW
 set INSTALL_ENV_DIR_CYGWIN=D:\cygwin
 
+SetLocal EnableDelayedExpansion
 if %INSIDE_UTILS_ENV_FLAG% EQU %INSIDE_UTILS_ENV_MINGW% (
     echo Init Included MinGW env.
-    set "PATH=%INSTALL_ENV_DIR_MINGW%\bin;%PATH%"
-    set "PATH=%INSTALL_ENV_DIR_MINGW%\msys\1.0\bin;%INSTALL_ENV_DIR_MINGW%\msys\1.0\local\bin;%PATH%"
+    set "PATH=%INSTALL_ENV_DIR_MINGW%\bin;!PATH!"
+    set "PATH=%INSTALL_ENV_DIR_MINGW%\msys\1.0\bin;%INSTALL_ENV_DIR_MINGW%\msys\1.0\local\bin;!PATH!"
 ) else (
   if %INSIDE_UTILS_ENV_FLAG% EQU %INSIDE_UTILS_ENV_CYGWIN% (
     echo Init Included Cygwin env.
-    set "PATH=%INSTALL_ENV_DIR_CYGWIN%\bin;%PATH%"
+    set "PATH=%INSTALL_ENV_DIR_CYGWIN%\bin;!PATH!"
   ) else (
     echo Init Included MinGW and Cygwin env.
-    set "PATH=%INSTALL_ENV_DIR_CYGWIN%\bin;%PATH%"
-    set "PATH=%INSTALL_ENV_DIR_MINGW%\bin;%PATH%"
-    set "PATH=%INSTALL_ENV_DIR_MINGW%\msys\1.0\bin;%INSTALL_ENV_DIR_MINGW%\msys\1.0\local\bin;%PATH%"
+    set "PATH=%INSTALL_ENV_DIR_CYGWIN%\bin;!PATH!"
+    set "PATH=%INSTALL_ENV_DIR_MINGW%\bin;!PATH!"
+    set "PATH=%INSTALL_ENV_DIR_MINGW%\msys\1.0\bin;%INSTALL_ENV_DIR_MINGW%\msys\1.0\local\bin;!PATH!"
   )
 )
+SetLocal DisableDelayedExpansion
+set PATH=%PATH%
+
 
 echo Init HOME directory to here call batfile.
 set HOME=%cd%
+set CC_HERE=%cd%
 
 REM ******************************
 REM Start ...
 REM ##############################
 
-if 1==1 (
+if "1" == "1" (
     echo "Build (unsigned):"
-    cd %HOME%/SayCV_OpenVPN/SayCV_OpenVPN_build/windows-nsis
+    cd %CC_HERE%/SayCV_OpenVPN/SayCV_OpenVPN_build/windows-nsis
     bash ./build-complete
 ) else (
     echo "Build (signed):"
-    cd %HOME%/SayCV_OpenVPN/SayCV_OpenVPN_build/windows-nsis
-    ./build-complete --sign --sign-pkcs12=<pkcs12-file> `
-    --sign-pkcs12-pass=<pkcs12-file-password> `
-    --sign-timestamp="<timestamp-url>"
+    cd %CC_HERE%/SayCV_OpenVPN/SayCV_OpenVPN_build/windows-nsis
+    bash ./build-complete --sign --sign-pkcs12=<pkcs12-file> --sign-pkcs12-pass=<pkcs12-file-password> --sign-timestamp="<timestamp-url>"
 )
 
 REM ##############################
@@ -85,6 +88,7 @@ if %EOF_ENV_FLAG% EQU %EOF_ENV_CMD% (
   goto :__subCall_EOF__
 
 :__subCall_CMD__
+  setlocal disabledelayedexpansion
   cmd
   goto :__subCall_EOF__
 
--- utils_callcmds_tpl.bat
+++ utils_callcmds_tpl.bat
@@ -24,29 +24,33 @@ set /a EOF_ENV_FLAG=0
 set INSTALL_ENV_DIR_MINGW=D:\MinGW
 set INSTALL_ENV_DIR_CYGWIN=D:\cygwin
 
+SetLocal EnableDelayedExpansion
 if %INSIDE_UTILS_ENV_FLAG% EQU %INSIDE_UTILS_ENV_MINGW% (
     echo Init Included MinGW env.
-    set "PATH=%INSTALL_ENV_DIR_MINGW%\bin;%PATH%"
-    set "PATH=%INSTALL_ENV_DIR_MINGW%\msys\1.0\bin;%INSTALL_ENV_DIR_MINGW%\msys\1.0\local\bin;%PATH%"
+    set "PATH=%INSTALL_ENV_DIR_MINGW%\bin;!PATH!"
+    set "PATH=%INSTALL_ENV_DIR_MINGW%\msys\1.0\bin;%INSTALL_ENV_DIR_MINGW%\msys\1.0\local\bin;!PATH!"
 ) else (
   if %INSIDE_UTILS_ENV_FLAG% EQU %INSIDE_UTILS_ENV_CYGWIN% (
     echo Init Included Cygwin env.
-    set "PATH=%INSTALL_ENV_DIR_CYGWIN%\bin;%PATH%"
+    set "PATH=%INSTALL_ENV_DIR_CYGWIN%\bin;!PATH!"
   ) else (
     echo Init Included MinGW and Cygwin env.
-    set "PATH=%INSTALL_ENV_DIR_CYGWIN%\bin;%PATH%"
-    set "PATH=%INSTALL_ENV_DIR_MINGW%\bin;%PATH%"
-    set "PATH=%INSTALL_ENV_DIR_MINGW%\msys\1.0\bin;%INSTALL_ENV_DIR_MINGW%\msys\1.0\local\bin;%PATH%"
+    set "PATH=%INSTALL_ENV_DIR_CYGWIN%\bin;!PATH!"
+    set "PATH=%INSTALL_ENV_DIR_MINGW%\bin;!PATH!"
+    set "PATH=%INSTALL_ENV_DIR_MINGW%\msys\1.0\bin;%INSTALL_ENV_DIR_MINGW%\msys\1.0\local\bin;!PATH!"
   )
 )
+SetLocal DisableDelayedExpansion
+set PATH=%PATH%
 
 echo Init HOME directory to here call batfile.
 set HOME=%cd%
 
+
 REM ******************************
 REM Start ...
 REM ##############################
-
+echo %PATH%
 
 REM ##############################
 REM End ...
