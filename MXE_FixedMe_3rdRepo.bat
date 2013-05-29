7e274adecc252210fd4084ccf7db19ccb3c92b88
SayCV <sssxyc@163.com>
2013-05-29 19:52:59 +0800

    fixedme : MXE_FixedMe_3rdRepo.bat

--- MXE_FixedMe_3rdRepo.bat
+++ MXE_FixedMe_3rdRepo.bat
@@ -195,7 +195,7 @@ git config --global --add https.proxy 127.0.0.1:8087
 git config --global --add http.sslVerify false
 git config --global --add https.sslVerify false
 :::#非常重要，否则可能提交不成功。
-:::git config --global http.postBuffer 524288000
+git config --global --add http.postBuffer 524288000
 
 
 REM ##############################
